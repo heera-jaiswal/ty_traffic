7a437641fb6b0dab428750841f5166137ccc59d0
heera-jaiswal <heera.jaiswal@travelyaari.com>
2015-04-03 16:39:39 +0530

    traffic to json hits

--- traffic_reader.py
+++ traffic_reader.py
@@ -7,7 +7,7 @@ def get_hits():
 	year=cur_date.strftime("%Y")
 	month=cur_date.strftime("%b")
 	path="/mnt/data/flume/logs/servers/ty/%s/%s/access-logs" %(year,month)
-	cmd="""grep "%s:" %s/%s|grep "%s" |tail|awk -F " " '{print $5}'""" %(cur_time,path,file_name,search_keyword)
+	cmd="""grep "%s:" %s/%s|grep "%s" |awk -F " " '{print $5}'""" %(cur_time,path,file_name,search_keyword)
 	print cmd
 	return execute(cmd)
 	
@@ -16,10 +16,35 @@ def execute(cmd):
 	from subprocess import check_output
 	return check_output(cmd,shell=True)
 
-def process_result(result):
-	pass
+def process_result(logs,city_list):
+	#/api/search/?mode=oneway&departDate=04-04-2015&fromCity=Coimbatore&toCity=Bangalore&pickups=1&_=142805861700
+	items=logs.split("\n")
+	result=[]
+	for item in items:
+		dict_item={}
+		for param in item.split("?")[1].split("&"):
+			key=param.split(0)
+			val=param.split(1)
+			dict_item[key]=val
+		if "fromCity" in dict_item and "toCity" in dict_item:
+			try:
+				from_id = city_list[dict_item["fromCity"].lower()]["cid"]
+				to_id = city_list[dict_item["toCity"].lower()]["cid"]
+				result.append({"from":from_id,"to":to_id})
+			except Exception as e:
+				pass
+	return result
 
 if __name__=="__main__":
 	#sl=execute("dir")
-	print get_hits()
+	import json
+	city_list_path="city_to_id.json"
+	city_list=None
+	with temp open(city_list_path,"rb") as t:
+		city_list=json.loads(t.read())
+	logs=get_hits()
+	result=process_result(logs,city_list)
+	print json.dumps({"hits":result},indent=4)
+	print len(result)
+
 	#print sl
